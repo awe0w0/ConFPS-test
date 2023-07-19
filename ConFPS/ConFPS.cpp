4bca71aae79b8cc0d2c985e12c7602e7d4f74cfe
小明 王 <awe0_0@outlook.com>
2023-07-20 02:39:25 +0800

    有点问题，明天再看

--- ConFPS/ConFPS.cpp
+++ ConFPS/ConFPS.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
-float fPlayerX = 0.0f;
-float fPlayerY = 0.0f;
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
 float fPlayerA = 0.0f;
 
 int nMapHeight = 16;
 int nMapWidth = 16;
 
 float fFOV = 3.14159 / 4.0;
+float fDepth = 16.0f;
 
 int main()
 {
@@ -46,7 +47,44 @@ int main()
 		for (int x = 0; x < nScreenWidth; x++) {
 			//For each column, calculate the projected ray angle into world space
 			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0;
+			bool bHitWall = false;
+
+			float fEyeX = sinf(fRayAngle); //Unit vector for ray in player space
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth) {
+				
+				fDistanceToWall += 0.1f;
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+				//Test if ray is out of bounds
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapWidth) {
+					bHitWall = true; //Just srt distance to maximum depth
+					fDistanceToWall = fDepth;
+				}
+				else {
+					//Ray is inbounds so test to see if the ray	cell is a wall block
+					if (map[nTestY * nMapWidth + nTestX] == '#') bHitWall = true;
+				}
+				
+			}
+			
+			//Calculate distance to ceiling and floor
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+
+			for (int y = 0; y < nScreenHeight; y++) {
+				if (y < nCeiling) screen[y * nScreenWidth + x] = ' ';
+				else if (y > nCeiling && y <= nFloor) screen[y * nScreenWidth + x] = '#';
+				else screen[y * nScreenWidth + x] = ' ';
+			}
+
 		}
+
+		
 
 		screen[nScreenHeight * nScreenWidth - 1] = '\0';
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenHeight * nScreenWidth, { 0,0 }, &dwBytesWritten);
